5ff17befbc7899ba482d07a2b723593f21e65229
Disha Mahna <dishamahna1612@gmail.com>
2021-01-08 15:37:30 +1300

    Local DB changed to connect to AWS RDS

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const Sequelize = require("sequelize");
 // Option 1: Passing a connection URI
 
-const sequelize = new Sequelize("social_notes", "postgres", "open", {
-  host: "localhost",
+const sequelize = new Sequelize("social_notes", "postgres", "12345678", {
+  host: "aa1g5yao73kurmg.cpumudghbgc2.us-east-2.rds.amazonaws.com",
   dialect: "postgres",
 });
 
 module.exports = sequelize;
+
+// https://github.com/dishamahna16/socialnotesAPI - bACKEND LINK
+
+// https://github.com/dishamahna16/Socialnotes_React - FRONTEDN LINK
